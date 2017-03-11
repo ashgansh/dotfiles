a06a4cde8a9f19b0a9b5b18c9d1040a778368d1a
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-03-11 10:22:02 +0100

    Small script to install vim and its plugins

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Setup VIM
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/backup_files
+mkdir -p ~/.vim/swap_files
+mkdir -p ~/.vim/undo_files
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
