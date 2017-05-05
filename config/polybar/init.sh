e1e7e9563d399af5f07f78f8cfaa2e193423e19a
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-05-05 08:38:35 +0200

    Add laptap config for polybar

--- config/polybar/init.sh
+++ config/polybar/init.sh
@@ -1,4 +1,11 @@
 killall -q polybar
+echo `hostname`
+
+if [[ `hostname` = laptap ]]
+then
+  MONITOR=eDP1 polybar top
+else
 MONITOR=DVI-D-0 polybar top &
 MONITOR=DVI-I-1 polybar top &
 MONITOR=HDMI-0  polybar top &
+fi
