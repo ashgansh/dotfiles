e417926220a0e620f998ea3994902d30e238a328
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-05-15 09:55:52 +0200

    Polybar: add custom args for desktop

--- config/polybar/init.sh
+++ config/polybar/init.sh
@@ -8,8 +8,11 @@ then
     export POLYBAR_HEIGHT=65
     export POLYBAR_TRAY_SIZE=35
     polybar top
-else
-  MONITOR=DVI-D-0 polybar top &
-  MONITOR=DVI-I-1 polybar top &
-  MONITOR=HDMI-0  polybar top &
-fi
+  else
+    export DPI=100
+    export POLYBAR_HEIGHT=30
+    export POLYBAR_TRAY_SIZE=18
+    MONITOR=DVI-D-0 polybar top &
+    MONITOR=DVI-I-1 polybar top &
+    MONITOR=HDMI-0  polybar top &
+  fi
