d62be660542b13f83f585cc3975fbc93ff0c3842
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-10-13 13:37:03 +0200

    Add support for dual screen

--- config/polybar/init.sh
+++ config/polybar/init.sh
@@ -3,7 +3,6 @@ echo `hostname`
 
 if [[ `hostname` = laptap ]]
 then
-    export MONITOR=eDP1
     export DPI=230
     export POLYBAR_HEIGHT=65
     export POLYBAR_TRAY_SIZE=35
@@ -16,3 +15,5 @@ then
     MONITOR=DVI-I-1 polybar top &
     MONITOR=HDMI-0  polybar top &
   fi
+    MONITOR=eDP1 polybar top &
+    MONITOR=DP1 polybar top &
