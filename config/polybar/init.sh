df5540ef805e8aba492365200685a55ef880ce7a
Benjamin Shafii <benjamin.shafii@gmail.com>
2017-11-26 14:39:18 +0000

    Modify polybar script to include fms-laptop

--- config/polybar/init.sh
+++ config/polybar/init.sh
@@ -1,19 +1,17 @@
 killall -q polybar
-echo `hostname`
 
 if [[ `hostname` = laptap ]]
 then
     export DPI=230
     export POLYBAR_HEIGHT=65
     export POLYBAR_TRAY_SIZE=35
-    polybar top
-  else
-    export DPI=100
-    export POLYBAR_HEIGHT=30
-    export POLYBAR_TRAY_SIZE=18
-    MONITOR=DVI-D-0 polybar top &
-    MONITOR=DVI-I-1 polybar top &
-    MONITOR=HDMI-0  polybar top &
-  fi
     MONITOR=eDP1 polybar top &
     MONITOR=DP1 polybar top &
+  elif [[ `hostname` = fms-laptop ]]
+  then
+    echo "hello"
+    export DPI=100
+    export POLYBAR_HEIGHT=24
+    MONITOR=eDP-1 polybar top &
+    MONITOR=HDMI-1 polybar top &
+  fi
